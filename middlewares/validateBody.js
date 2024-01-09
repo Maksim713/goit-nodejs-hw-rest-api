a207c7648fa33c469d3a102f21bcd2fc7c00b302
Maxim Sytnik <118841889+Maksim713@users.noreply.github.com>
2024-01-09 09:38:57 +0200

    fix

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,8 +1,11 @@
+const Joi = require("joi");
 const { HttpError } = require("../helpers");
 
 const validateBody = (schema) => {
+  const compiledSchema = Joi.compile(schema);
+
   const func = (req, res, next) => {
-    const { error } = schema.validate(req.body);
+    const { error } = compiledSchema.validate(req.body);
     if (error) {
       return next(HttpError(400, error.message));
     }
